e1eb4e3b664b8336b12f3d1b0ae9d2697aea1805
faycalelh <152806576+faycalelh@users.noreply.github.com>
2026-01-03 00:00:29 +0100

    Producteur Consomateur static halt

--- src/ProducteurConsomateur/MyMain.java
+++ src/ProducteurConsomateur/MyMain.java
@@ -2,12 +2,15 @@ package ProducteurConsomateur;
 
 public class MyMain {
     public static void main(String[] args) throws InterruptedException {
+
         File f = new File(10);
+
         Consomateur c1 = new Consomateur("c1",f);
         Consomateur c2 = new Consomateur("c2",f);
         Producteur p1 = new Producteur("p1", f);
         Producteur p2 = new Producteur("p2", f);
         Producteur p3 = new Producteur("p3", f);
+
         Thread t1 = new Thread(p1);
         Thread t2 = new Thread(p2);
         Thread t3 = new Thread(p3);
